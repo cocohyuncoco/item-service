5611ea951547fa658cfe4abee69d5011704f5be9
cocohyuncoco <guswjd8287@gmail.com>
2022-09-12 23:39:12 +0900

    [임현정] 타임리프

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,41 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor // final이 붙은 객체를 가지고 생성자를 만들어줌
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+//    @RequiredArgsConstructor  으로 자동생성 가능하게됨
+//    public BasicItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
+
+     @GetMapping
+     public String items(Model model){
+         List<Item> items = itemRepository.findAll();
+         model.addAttribute("items", items);
+         init();
+         return "basic/items";
+     }
+
+     /* 테스트용 데이터 추가 */
+     @PostMapping
+     public void init(){
+         itemRepository.save(new Item("itemA", 10000, 10));
+         itemRepository.save(new Item("itemB", 20000, 20));
+     }
+}
+
